c0a773bbd6c98f43a05208f1d8290e0d969f0794
Gonzalez75 <guilherme75gonzalez@gmail.com>
2022-08-11 21:41:30 -0300

    commit #2

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const transactionsUl = document.querySelector('#transactions')
+
+const dummyTransactions = [
+    {id: 1, name: 'Bolo', amount: -20},
+    {id: 2, name: 'Salario', amount: 300},
+    {id: 3, name: 'Torta', amount: -10},
+    {id: 4, name: 'Violao', amount: 150}
+]
+
+const addTransactionsIntoDOM = transaction => {
+    const operator = transaction.amount < 0 ? '-' : '+'
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+    const amountWihoutOperator = Math.abs(transaction.amount)
+    const li = document.createElement('li')
+
+    li.classList.add(CSSClass)
+    li.innerHTML = `
+    ${transaction.name} <span>${operator} R$ ${amountWihoutOperator}</span><button class="delete-btn">x</button>
+    `
+    transactionsUl.prepend(li)
+}
