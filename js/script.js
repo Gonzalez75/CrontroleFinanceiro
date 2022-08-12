f28960fcf976a152d9359d9b1a2b288bbfcadcc7
Gonzalez75 <guilherme75gonzalez@gmail.com>
2022-08-11 22:05:57 -0300

    commit #4

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 const transactionsUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
 
 const dummyTransactions = [
     {id: 1, name: 'Bolo', amount: -20},
@@ -21,9 +24,25 @@ const addTransactionsIntoDOM = transaction => {
 }
 
 const updateBalanceValues = () => {
-    const transactionAmounts = dummyTransactions.map(transaction => transaction.amount)
-    console.log(transactionAmounts)
-}
+    const transactionAmounts = dummyTransactions
+        .map(transaction => transaction.amount)
+    const total = transactionAmounts
+        .reduce((accumulator, transaction) => accumulator + transaction, 0)
+        .toFixed(2)
+    const income = transactionAmounts
+        .filter(value => value > 0)
+        .reduce((accumulator, value) => accumulator + value, 0)
+        .toFixed(2)
+
+    const expense = Math.abs(transactionAmounts
+        .filter(value => value < 0)
+        .reduce((accumulator, value) => accumulator + value, 0))
+        .toFixed(2)
+
+        balanceDisplay.textContent = `R$ ${total}`
+        incomeDisplay.textContent = `R$ ${income}`
+        expenseDisplay.textContent = `R$ ${expense}`
+    }
 
 const init = () => {
     dummyTransactions.forEach(addTransactionsIntoDOM)
